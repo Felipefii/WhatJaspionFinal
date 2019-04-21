33c419ae5732286e7449b9a9af6b0c3d0e9be722
Felipefii <093928fan>
2019-04-21 19:10:04 -0300

    Funcionalidades completas: - Cadastro; - Login; - Logout.

--- app/src/main/java/com/whatjaspionfinal/activity/MainActivity.java
+++ app/src/main/java/com/whatjaspionfinal/activity/MainActivity.java
@@ -3,20 +3,57 @@ package com.whatjaspionfinal.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.whatjaspionfinal.R;
+import com.whatjaspionfinal.config.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("WhatJaspion");
         setSupportActionBar( toolbar );
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuSair:
+                deslogarUsuario();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void deslogarUsuario(){
+        try {
+            autenticacao.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
