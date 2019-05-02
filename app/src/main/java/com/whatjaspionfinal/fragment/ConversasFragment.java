086659e7d60c1677614b3544403bb847b50b4ace
Felipefii <093928fan>
2019-05-01 21:17:23 -0300

    Funcionalidades completas: - evento ClickListener em conversas para abrir chat com contatos

--- app/src/main/java/com/whatjaspionfinal/fragment/ConversasFragment.java
+++ app/src/main/java/com/whatjaspionfinal/fragment/ConversasFragment.java
@@ -1,6 +1,7 @@
 package com.whatjaspionfinal.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,16 +11,20 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.whatjaspionfinal.R;
+import com.whatjaspionfinal.activity.ChatActivity;
 import com.whatjaspionfinal.adapter.ConversasAdapter;
 import com.whatjaspionfinal.config.ConfiguracaoFirebase;
+import com.whatjaspionfinal.helper.RecyclerItemClickListener;
 import com.whatjaspionfinal.helper.UsuarioFireBase;
 import com.whatjaspionfinal.model.Conversa;
+import com.whatjaspionfinal.model.Usuario;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -60,6 +65,34 @@ public class ConversasFragment extends Fragment {
         recyclerViewConversas.setLayoutManager(layoutManager);
         recyclerViewConversas.setHasFixedSize(true);
         recyclerViewConversas.setAdapter(adapter);
+
+        recyclerViewConversas.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getActivity(),
+                        recyclerViewConversas,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+
+                                Conversa conversaSelecionada = listaConversas.get(position);
+
+                                Intent i = new Intent(getActivity(), ChatActivity.class);
+                                i.putExtra("chatContatos", conversaSelecionada.getUsuarioExibicao());
+                                startActivity(i);
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            }
+                        }
+                )
+        );
 
         return view;
     }
